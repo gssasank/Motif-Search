868424984384eab2950be791462596a67418cefa
Sasank GS <gssasank@gmail.com>
2021-12-05 01:55:07 +0530

    Wrote the algorithm for calculating the Edit Distance using DP principles

--- test.py
+++ test.py
@@ -13,41 +13,31 @@ for _ in range(20):
     random_string_list.append(random_string)
 
 
-# Algorithm for Edit Distance using dynamic programming
-def edit_distance_dp(x, y):
+# Algorithm for Edit Distance using dynamic programming 
+def edit_distance_dp(x, y, indel, sub):
     m = len(x)
     n = len(y)
 
     # Create a table to store results of subproblems
-    dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    E = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
-    # Fill d[][] in bottom up manner
+    for i in range(m+1):
+        E[i][0] = i 
+
+    for j in range(n+1):
+        E[0][j] = j
+
+
     for i in range(m + 1):
         for j in range(n + 1):
 
-            # If first string is empty, only option is to
-            # insert all characters of second string
-            if i == 0:
-                dp[i][j] = j    # Min. operations = j
+            clap = 0 if x[i] == y[j] else sub
 
-            # If second string is empty, only option is to
-            # remove all characters of second string
-            elif j == 0:
-                dp[i][j] = i    # Min. operations = i
+            E[i][j] = 1 + min(E[i][j-1] + indel,        # Insert
+                                E[i-1][j] + indel,        # Remove
+                                E[i-1][j-1] + clap)    # Replace
 
-            # If last characters are same, ignore last char
-            # and recur for remaining string
-            elif x[i-1] == y[j-1]:
-                dp[i][j] = dp[i-1][j-1]
-
-            # If last character are different, consider all
-            # possibilities and find minimum
-            else:
-                dp[i][j] = 1 + min(dp[i][j-1],        # Insert
-                                   dp[i-1][j],        # Remove
-                                   dp[i-1][j-1])    # Replace
-
-    return dp[m][n]
+    return(E[m][n])
 
 # print(random_string_list)
 
@@ -59,12 +49,6 @@ def edit_distance_dp(x, y):
 #           where a string M’ is considered as a Neighbor of M if Edit distance (M, M’) <= D.
 
 
-# for i in range(len(random_string_list)):
-#     for j in range(len(random_string_list)):
-#         if i != j:
-#             for k in range(len(random_string_list[i])):
-#                 if random_string_list[i][k:k+L] == random_string_list[j][k:k+L]:
-#                     print(random_string_list[i][k:k+L])
 
 for string in random_string_list:
     list_of_substrings = [string[i:i+L] for i in range(586)] # this is because, only 585 sub-strings of length 15 will exist.
